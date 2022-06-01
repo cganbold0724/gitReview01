65889197d09ddd2032b519f3fa818162e2658034
cganbold0724 <cganbold0724@gmail.com>
2022-05-31 21:52:36 -0400

    2nd commit

--- src/gitReview/gitreview01.java
+++ src/gitReview/gitreview01.java
@@ -5,6 +5,10 @@ public class gitreview01 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		System.out.print("today we learn github");
+		System.out.print("today is a very good DAY");
+
+		System.out.print("today is a very good DAY");
 
 	}
 
